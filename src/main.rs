80c6c82f09b5fd8aa8fccc9bb434644035ed8108
Zak Farmer <zak@3sidedcube.com>
2022-08-27 00:58:01 +0100

    Implemented Coulomb's law for electrostatic force

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,17 @@ extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
 
+use constants::{COULOMB_CONSTANT, ELECTRON_CHARGE, ELECTRON_MASS};
 use glutin_window::GlutinWindow as Window;
+use graphics::math::{add, mul_scalar};
 use opengl_graphics::{GlGraphics, OpenGL};
 use particle::Particle;
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
 use rand::Rng;
+
+use crate::constants::PHYSICS_SCALE;
 
 pub struct App {
     gl: GlGraphics,
@@ -22,12 +26,17 @@ pub struct App {
 impl App {
     fn init(&mut self, particle_num: i32) {
         let mut rng = rand::thread_rng();
+
         self.particles = Vec::new();
 
         for _ in 0..particle_num {
             self.particles.push(Particle::new(
-                [rng.gen_range(0.0..800.0), rng.gen_range(0.0..800.0)],
-                [rng.gen_range(-100.0..100.0), rng.gen_range(-100.0..100.0)],
+                [0.0, 0.0],
+                [rng.gen_range(10.0..800.0), rng.gen_range(10.0..800.0)], //[rng.gen_range(0.0..800.0), rng.gen_range(0.0..800.0)],
+                [rng.gen_range(-5.0..5.0), rng.gen_range(-5.0..5.0)],
+                rng.gen_range(1.0..10.0) * ELECTRON_MASS,
+                ELECTRON_CHARGE * rng.gen_range(1..5) as f64,
+                [1.0, 1.0, 1.0, rng.gen_range(0.0..1.0) as f32],
             ));
         }
     }
@@ -35,40 +44,97 @@ impl App {
     fn render(&mut self, args: &RenderArgs) {
         use graphics::*;
 
-        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
-        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-
-        let body = ellipse::circle(0.0, 0.0, 8.0);
+        let body = ellipse::circle(0.0, 0.0, 1.0);
 
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen
-            clear(BLACK, gl);
+            clear([0.0, 0.0, 0.0, 1.0], gl);
 
             for i in 0..self.particles.len() {
                 let transform = c
                     .transform
                     .trans(self.particles[i].position[0], self.particles[i].position[1]);
 
-                ellipse(WHITE, body, transform, gl);
+                ellipse(self.particles[i].colour, body, transform, gl);
             }
-        })
+        });
     }
 
     fn update(&mut self, args: &UpdateArgs) {
-        for i in 0..self.particles.len() {
-            self.particles[i].update(args.dt);
+        for a in 0..self.particles.len() {
+            self.particles[a].update(args.dt);
 
-            for n in 0..self.particles.len() {
-                if i != n {
-                    let dx = self.particles[i].position[0] - self.particles[n].position[0];
-                    let dy = self.particles[i].position[1] - self.particles[n].position[1];
-                    let dist = ((dx * dx) + (dy * dy)).sqrt();
+            for b in 0..self.particles.len() {
+                if a != b {
+                    // Create distance vector for particle A
+                    let distance_a = [
+                        self.particles[a].position[0] - self.particles[b].position[0],
+                        self.particles[a].position[1] - self.particles[b].position[1],
+                    ];
 
-                    if dist < 8.0 {
-                        self.particles[i].velocity[0] *= -1.0;
-                        self.particles[i].velocity[1] *= -1.0;
-                        self.particles[n].velocity[0] *= -1.0;
-                        self.particles[n].velocity[1] *= -1.0;
+                    // Create distance vector for particle B
+                    let distance_b: [f64; 2] = [
+                        self.particles[b].position[0] - self.particles[a].position[0],
+                        self.particles[b].position[1] - self.particles[a].position[1],
+                    ];
+
+                    // Coulomb's law - F = k * q1 * q2 / r^2
+                    let force_a: [f64; 2] = [
+                        COULOMB_CONSTANT * self.particles[a].charge * self.particles[b].charge
+                            / (distance_a[0] * distance_a[0]),
+                        COULOMB_CONSTANT * self.particles[a].charge * self.particles[b].charge
+                            / (distance_a[1] * distance_a[1]),
+                    ];
+
+                    let force_b: [f64; 2] = [
+                        COULOMB_CONSTANT * self.particles[b].charge * self.particles[a].charge
+                            / (distance_b[0] * distance_b[0]),
+                        COULOMB_CONSTANT * self.particles[b].charge * self.particles[a].charge
+                            / (distance_b[1] * distance_b[1]),
+                    ];
+
+                    let acceleration_a: [f64; 2] = [
+                        force_a[0] / self.particles[a].mass,
+                        force_a[1] / self.particles[a].mass,
+                    ];
+
+                    let acceleration_b: [f64; 2] = [
+                        force_b[0] / self.particles[b].mass,
+                        force_b[1] / self.particles[b].mass,
+                    ];
+
+                    self.particles[a].acceleration = mul_scalar(
+                        add(self.particles[a].acceleration, acceleration_a),
+                        PHYSICS_SCALE,
+                    );
+
+                    self.particles[b].acceleration = mul_scalar(
+                        add(self.particles[b].acceleration, acceleration_b),
+                        PHYSICS_SCALE,
+                    );
+
+                    if self.particles[a].acceleration[0].is_nan()
+                        || self.particles[a].acceleration[0].is_infinite()
+                    {
+                        self.particles[a].acceleration[0] = 0.0;
+                    }
+
+                    if self.particles[a].acceleration[1].is_nan()
+                        || self.particles[a].acceleration[1].is_infinite()
+                    {
+                        self.particles[a].acceleration[1] = 0.0;
+                    }
+
+                    if self.particles[b].acceleration[0].is_nan()
+                        || self.particles[b].acceleration[0].is_infinite()
+                    {
+                        self.particles[b].acceleration[0] = 0.0;
+                    }
+
+                    if self.particles[b].acceleration[1].is_nan()
+                        || self.particles[b].acceleration[1].is_infinite()
+                    {
+                        self.particles[b].acceleration[1] = 0.0;
                     }
                 }
             }
@@ -90,7 +156,7 @@ fn main() {
         particles: Vec::new(),
     };
 
-    app.init(100);
+    app.init(1000);
 
     let mut events = Events::new(EventSettings::new());
 
--- src/particle.rs
+++ src/particle.rs
@@ -1,40 +1,62 @@
 use graphics::math::{add, mul_scalar};
 use graphics::types::Vec2d;
 
-use crate::constants::DAMPING_FACTOR;
+use crate::constants::{DAMPING_FACTOR, TIMESCALE};
+
+//use crate::constants::DAMPING_FACTOR;
 
 pub struct Particle {
-    pub position: Vec2d,
-    pub velocity: Vec2d,
+    pub acceleration: Vec2d, // 2D vector for acceleration
+    pub position: Vec2d,     // 2D vector for position
+    pub velocity: Vec2d,     // 2D vector for velocity
+    pub mass: f64,           // Mass of particle
+    pub charge: f64,         // Charge of the particle (in coulombs)
+    pub colour: [f32; 4],    // Colour of the particle
 }
 
 impl Particle {
-    pub fn new(position: Vec2d, velocity: Vec2d) -> Particle {
+    pub fn new(
+        acceleration: Vec2d,
+        position: Vec2d,
+        velocity: Vec2d,
+        mass: f64,
+        charge: f64,
+        colour: [f32; 4],
+    ) -> Particle {
         Particle {
+            acceleration: acceleration,
             position: position,
             velocity: velocity,
+            mass: mass,
+            charge: charge,
+            colour: colour,
         }
     }
 
     pub fn update(&mut self, dt: f64) {
-        self.position = add(self.position, mul_scalar(self.velocity, dt));
+        self.velocity = add(self.velocity, mul_scalar(self.acceleration, dt));
+        self.position = add(self.position, mul_scalar(self.velocity, dt * TIMESCALE));
 
         if self.position[1] < 0.0 {
+            self.acceleration[1] = 0.0;
             self.position[1] = 0.0;
             self.velocity[1] = -self.velocity[1] * DAMPING_FACTOR;
         }
 
         if self.position[0] < 0.0 {
+            self.acceleration[0] = 0.0;
             self.position[0] = 0.0;
             self.velocity[0] = -self.velocity[0] * DAMPING_FACTOR;
         }
 
         if self.position[0] > 800.0 {
+            self.acceleration[0] = 0.0;
             self.position[0] = 800.0;
             self.velocity[0] = -self.velocity[0] * DAMPING_FACTOR;
         }
 
         if self.position[1] > 800.0 {
+            self.acceleration[1] = 0.0;
             self.position[1] = 800.0;
             self.velocity[1] = -self.velocity[1] * DAMPING_FACTOR;
         }
