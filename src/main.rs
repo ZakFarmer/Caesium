a64854f614ec7f5cba4b7db22662b6ebdbdf9cf0
Zak Farmer <zak@3sidedcube.com>
2022-09-01 20:20:02 +0100

    Sort of implemented the weak force

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,19 @@ use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
-use rand::Rng;
+use rand::{random, Rng};
 use rust_particles::physics::generators::generate_particle;
 use rust_particles::physics::nbody::step_simulation;
 use rust_particles::physics::simulation::{Simulation3D, SimulationConfig3D};
 use rust_particles::physics::vector::{Vector, Vector3D};
 
 const ELECTRON_CHARGE: f32 = -1.602_176_6e-19;
+const ELECTRON_MASS: f32 = 9.1093837 * 10e-31;
+
+const PROTON_MASS: f32 = 1.67262192 * 10e-27;
 
 const PARTICLE_COUNT: usize = 500;
-const PARTICLE_WIDTH_FACTOR: f32 = 1.0;
+const PARTICLE_WIDTH_FACTOR: f32 = 0.5 * 1e-30;
 
 pub struct App<'a> {
     gl: GlGraphics,
@@ -67,10 +70,18 @@ fn main() {
 
     for i in 0..sim.number {
         // Randomise whether this is a proton or an electron
-        let charge: f32 = if rng.gen_range(-0..100) < 50 {
+        let random_num = rng.gen_range(0..100);
+
+        let charge: f32 = if random_num < 50 {
             -ELECTRON_CHARGE
         } else {
             ELECTRON_CHARGE
+        };
+
+        let mass: f32 = if random_num < 50 {
+            PROTON_MASS
+        } else {
+            ELECTRON_MASS
         };
 
         sim.set(
@@ -78,7 +89,7 @@ fn main() {
             &generate_particle(
                 rng.gen_range(0..max_position.x as usize) as f32,
                 rng.gen_range(0..max_position.y as usize) as f32,
-                rng.gen_range(20.0..50.0 * 1e3),
+                mass,
                 charge,
             ),
         );
--- src/physics/nbody.rs
+++ src/physics/nbody.rs
@@ -5,6 +5,8 @@ use super::{
     vector::{Vector, Vector3D},
 };
 
+const K0: f32 = 8.9875517923 * 10e9;
+
 pub fn step_simulation(sim: &mut Simulation3D, dt: f32, theta: f32) {
     let (min_x, min_y) = sim.config.min_r.to_xy();
     let (max_x, max_y) = sim.config.max_r.to_xy();
@@ -36,6 +38,7 @@ pub fn step_simulation(sim: &mut Simulation3D, dt: f32, theta: f32) {
                 z: 0.0,
             };
 
+            let angle: f32 = f32::atan(distance.y / distance.x);
             let distance_sqrd: f32 = distance.l2_sqrd();
 
             if distance_sqrd < sim.config.min_dist_sqrd {
@@ -44,6 +47,12 @@ pub fn step_simulation(sim: &mut Simulation3D, dt: f32, theta: f32) {
 
             let inverse_distance_cubed: f32 = 1.0 / distance_sqrd.powf(3.0);
             sim.acceleration[i] += distance * node.mass * inverse_distance_cubed;
+
+            // CALCULATE ELECTROSTATIC FORCE
+            let f: f32 = (K0 * node.charge * sim.charge[i]) / distance_sqrd;
+
+            sim.acceleration[i].x += (f * f32::cos(angle)) / sim.mass[i];
+            sim.acceleration[i].y += (f * f32::sin(angle)) / sim.mass[i];
         }
     }
 
